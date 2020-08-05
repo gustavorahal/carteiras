e64999a102525f35f2b4aef2d26d23c6cbb4fdd5
Gustavo Rahal <grahal@me.com>
2020-08-05 14:16:10 -0300

    Ajustes

--- app/controllers/carteira_ativos_controller.rb
+++
@@ -1,8 +0,0 @@
-class CarteiraAtivosController < ApplicationController
-
-  def index
-    @carteira = Carteira.find params[:carteira_id]
-    @carteira_ativos = CarteiraAtivo.joins(:ativo).where(carteira_id: @carteira.id, valido: true).order(:book)
-    @books_porcentagem = CarteiraAtivo.where(carteira: @carteira.id, valido: true).group(:book).order(:book).sum(:porcentagem)
-  end
-end
--- app/models/carteira.rb
+++ app/models/carteira.rb
@@ -2,4 +2,8 @@ class Carteira < ApplicationRecord
   has_many :carteira_ativos
   belongs_to :investidor
 
+  def books_porcentagem
+    carteira_ativos.where(valido: true).group(:book).order(:book).sum(:porcentagem)
+  end
+
 end

35c78d729d64071931985a8dbad821ee5ac7fe83
Gustavo Rahal <grahal@me.com>
2020-09-06 21:34:00 -0300

    Update 20200906204726_remove_columns_of_extratos.rb

--- db/migrate/20200906204726_remove_columns_of_extratos.rb
+++ db/migrate/20200906204726_remove_columns_of_extratos.rb
@@ -1,20 +1,5 @@
 class RemoveColumnsOfExtratos < ActiveRecord::Migration[6.0]
   def change
-
-    Extrato.distinct.pluck(:investidor_id, :corretora_id, :moeda).each do |investidor_id, corretora_id, moeda|
-      ContaCorrente.create!(investidor_id: investidor_id,
-                            corretora_id: corretora_id,
-                            moeda: moeda)
-    end
-
-    Extrato.all.each do |ex|
-      cc = ContaCorrente.find_by(investidor_id: ex.investidor_id,
-                            corretora_id: ex.corretora_id,
-                            moeda: ex.moeda)
-      ex.conta_corrente = cc
-      ex.save!
-    end
-
     change_column_null :extratos, :conta_corrente_id, false
     remove_column :extratos, :investidor_id
     remove_column :extratos, :corretora_id

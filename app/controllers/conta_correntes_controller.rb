5fa251bede814d055a113da17fe60448c453f72b
Gustavo Rahal <gustavorahal@gmail.com>
2021-03-18 11:41:09 -0300

    Suporte a adição de CC

--- app/controllers/conta_correntes_controller.rb
+++ app/controllers/conta_correntes_controller.rb
@@ -1,14 +1,54 @@
 class ContaCorrentesController < ApplicationController
 
+  before_action :set_vars, only: [:index, :new, :create, :edit, :update]
+
   def index
-    @carteira = Carteira.find params[:carteira_id]
     @conta_correntes = ContaCorrente.includes(:corretora).where(carteira: @carteira)
   end
 
   def show
     @conta_corrente = ContaCorrente.find params[:id]
-    @extratos = @conta_corrente.extratos.where("liquidacao::date <= '#{@data}'").order(liquidacao: :desc)
+    @extratos = @conta_corrente.extratos.where("liquidacao::date <= '#{@data}'").order(liquidacao: :desc, created_at: :desc)
     @carteira = @conta_corrente.carteira
+  end
+
+  def new
+    @conta_corrente = ContaCorrente.new
+  end
+
+  def create
+    @conta_corrente = ContaCorrente.new secure_params
+
+    if @conta_corrente.save
+      redirect_to carteira_conta_corrente_path(@carteira, @conta_corrente)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @conta_corrente = ContaCorrente.find params[:id]
+  end
+
+  def update
+    @conta_corrente = ContaCorrente.find params[:id]
+    if @conta_corrente.update(secure_params)
+      redirect_to carteira_conta_corrente_path(@carteira, @conta_corrente),
+                                          notice: "Conta Corrente atualizada com sucesso!"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_vars
+    @corretoras = Corretora.all.order(:nome)
+    @carteira = Carteira.find params[:carteira_id]
+  end
+
+  def secure_params
+    params.require(:conta_corrente).permit(:corretora_id, :carteira_id, :moeda)
   end
 
 end

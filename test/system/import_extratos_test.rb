e2429a13466466cde706b8d790edfabd6130ceda
Gustavo Rahal <gustavorahal@gmail.com>
2021-03-30 16:37:52 -0300

    Test importa extrato Vitreo

--- test/system/import_extratos_test.rb
+++ test/system/import_extratos_test.rb
@@ -1,20 +1,12 @@
 require 'application_system_test_case'
 
 class ImportExtratosTest < ApplicationSystemTestCase
-  # test 'importa extrato Vitreo' do
-  #   cc = conta_correntes(:vitreo_brl)
-  #   carteira = cc.carteira
-  #
-  #   visit carteira_conta_corrente_path carteira, cc
-  #
-  #   assert_selector 'h2', text: "Conta Corrente #{cc.corretora.nome} (#{cc.moeda})"
-  #
-  #   attach_file('Importar extrato', file_path('extrato-vitreo.xls'), make_visible: true) # arquivo de extrato bugado
-  #
-  #   click_on 'Importar'
-  #
-  #   assert_selector 'td', text: 'NOTA CORRETAGEM 123382 PREGÃO 17/03/2021'
-  # end
+  test 'importa extrato Vitreo' do
+    _test_importa_extrato(:vitreo_brl, 'extrato_vitreo.csv',
+                          ['Pagamento de Frações CSAN3',
+                           'TED BCO 341 AGE 8294 CTA 04463 4 - CREDITO EM C/C',
+                           '-R$ 24.745,41'])
+  end
 
   test 'importa extrato XP' do
     _test_importa_extrato(:xp_brl, 'extrato_xp.xlsx',

ab5b5b409a70a5bbb3bcc8e1f46e5276fe991c57
Gustavo Rahal <gustavorahal@gmail.com>
2021-04-12 19:33:47 -0300

    Update provento.rb

--- app/models/provento.rb
+++ app/models/provento.rb
@@ -11,11 +11,18 @@ class Provento < ApplicationRecord
   }
 
   def self.mes_a_mes(evento = nil)
-    if evento
-      where(evento: evento).group(Arel.sql("DATE_TRUNC('month', data)")).order(Arel.sql("DATE_TRUNC('month', data)")).sum(:valor_liquido)
-    else
-      group(Arel.sql("DATE_TRUNC('month', data)")).order(Arel.sql("DATE_TRUNC('month', data)")).sum(:valor_liquido)
+    query = group(Arel.sql("DATE_TRUNC('month', data)")).order(Arel.sql("DATE_TRUNC('month', data)"))
+    query = query.where(evento: evento) if evento
+
+    resultado = query.sum(:valor_liquido)
+    # DATE_TRUNC retorna inicio do mês quando na realidade a somatorio representa o final,
+    # Além disso só queremos a data, remover hora... corrigir
+    resultado_fix = {}
+    resultado.each do |data, valor|
+      resultado_fix[data.end_of_month.to_date] = valor
     end
+
+    resultado_fix
   end
 
 end

0e82978363f7e234352232f45ae26a81df86459b
Gustavo Rahal <gustavorahal@gmail.com>
2021-04-15 19:00:13 -0300

    Update cotacao_service.rb

--- app/lib/cotacao_service.rb
+++ app/lib/cotacao_service.rb
@@ -4,7 +4,14 @@ class CotacaoService
   # param @data: irá tentar buscar cotacao para data dada. Se não for possivel, retornar
   #              ultimo dia antes da data com uma cotacao disponivel
   def self.cotacao(ativo, data)
-    Rails.cache.fetch("cotacao_ativo_ID#{ativo.id}_#{data}") do
+    # Porque expirar o cache para dado ativo em determinada data já que é algo que não mudaria?
+    # R. Se pedirmos um cotacao para um ativo "hoje", vamos receber a cotacao de ontem ou anteontem
+    # porque nosso backend só tem dados com mais de 1 dia de atraso.
+    # Passados alguns dias, se pedirmos a cotacao para o mesmo ativo na mesma data, iriamos receber
+    # a cotacao correta, do dia exato pedido. Nesse sentido, não podemos gravar o cache indefinidamente.
+    # A cotacao de "hoje" esta bom para hoje, passado um tempo, queremos a cotacao correta, ou seja
+    # da data efetivamente pedida.
+    Rails.cache.fetch("cotacao_ativo_ID#{ativo.id}_#{data}", expires: 12.hours) do
       _resolve_cotacao(ativo, data)
     end
   end

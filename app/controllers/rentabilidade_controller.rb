14f6ba7a316530056e3663d6dc2d737174d7be11
Gustavo Rahal <gustavorahal@gmail.com>
2021-04-29 17:44:59 -0300

    Update rentabilidade_controller.rb

--- app/controllers/rentabilidade_controller.rb
+++ app/controllers/rentabilidade_controller.rb
@@ -4,12 +4,12 @@ class RentabilidadeController < ApplicationController
     @carteira = Carteira.find params[:carteira_id]
     @ultimos_carteira_ativos = []
     corretoras_tmp = []
-    ultimo_dia_mes_anterior = Date.today.prev_month.end_of_month
+    ate_dia_do_mes = Utils.ultimo_dia_util Date.today
     # ultimos 8 meses para não pesar
     8.times do
-      ca = CarteiraAtivos.new(@carteira, ultimo_dia_mes_anterior)
+      ca = CarteiraAtivos.new(@carteira, ate_dia_do_mes)
       @ultimos_carteira_ativos.push ca
-      ultimo_dia_mes_anterior = ultimo_dia_mes_anterior.prev_month.end_of_month
+      ate_dia_do_mes = ate_dia_do_mes.prev_month.end_of_month
       corretoras_tmp += ca.corretoras
     end
     @corretoras = corretoras_tmp.uniq

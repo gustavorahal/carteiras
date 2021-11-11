d3ee730dd7e638bfd95defd90e59ade99afcf2b6
Gustavo Rahal <gustavorahal@gmail.com>
2021-11-11 14:52:31 -0300

    Leve em consideração a moeda para calcular movimentacao mes a mes

--- app/models/movimentacao.rb
+++ app/models/movimentacao.rb
@@ -4,19 +4,28 @@ class Movimentacao < ApplicationRecord
   belongs_to :extrato, optional: true
 
   def self.mes_a_mes(corretora = nil)
-    query = group(Arel.sql("DATE_TRUNC('month', data)")).order(Arel.sql("DATE_TRUNC('month', data)"))
+    query = order(data: :asc)
     if corretora
       query = query.where(corretora: corretora)
     end
-    resultado = query.sum(:valor)
-    # DATE_TRUNC retorna inicio do mês quando na realidade a somatorio representa o final,
-    # Além disso só queremos a data, remover hora... corrigir
-    resultado_fix = {}
-    resultado.each do |data, valor|
-      resultado_fix[data.end_of_month.to_date] = valor
+
+    resultado = {}
+    query.each do |row|
+      if row.moeda == 'USD'
+        valor = row.valor * CotacaoService.moedas('USDBRL', row.data)
+      else
+        valor = row.valor
+      end
+
+      current_month = row.data.end_of_month.to_date
+      if resultado[current_month].present?
+        resultado[current_month] += valor
+      else
+        resultado[current_month] = valor
+      end
     end
 
-    resultado_fix
+    resultado
   end
 
   def self.total

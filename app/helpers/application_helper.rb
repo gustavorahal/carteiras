285d32790eda78cec2ba1925b247c52f8469a779
Gustavo Rahal <gustavorahal@gmail.com>
2021-11-14 11:37:52 -0300

    Fix e upgrade do bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,7 +30,7 @@ module ApplicationHelper
     unidade = 'R$'
     unidade = 'US$' if moeda == 'USD'
 
-    valor_currency = number_to_currency valor, unit: unidade
+    valor_currency = variable_precision_currency valor, 2, unidade
     css_color = ""
 
     if cores
@@ -42,6 +42,14 @@ module ApplicationHelper
     end
 
     "<span class=\"#{css_color}\">#{valor_currency}</span>".html_safe
+  end
+
+  # https://stackoverflow.com/questions/3320051/using-a-dynamic-precision-value-in-number-to-currency-based-on-the-decimal-value
+  def variable_precision_currency(valor, min_precision, unidade)
+    num = BigDecimal(valor.to_s)
+    prec = (num - num.floor).to_s.length - 2
+    prec = min_precision if prec < min_precision
+    number_to_currency(num, precision: prec, unit: unidade)
   end
 
 end

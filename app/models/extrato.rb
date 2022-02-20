634202fc720b88dd363755c6f2a6d1bc4e7a9e1d
Gustavo Rahal <gustavorahal@gmail.com>
2022-02-20 13:56:08 -0300

    Update extrato.rb

--- app/models/extrato.rb
+++ app/models/extrato.rb
@@ -1,5 +1,7 @@
 class Extrato < ApplicationRecord
   belongs_to :conta_corrente
+  has_one :provento, dependent: :destroy
+  has_one :movimentacao, dependent: :destroy
 
   def to_s
     "Extrato ID##{id}"
